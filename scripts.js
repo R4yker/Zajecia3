7b2f3f48ec6cee18133dab996b366b15860a43a9
R4yker <55552420+R4yker@users.noreply.github.com>
2019-10-03 19:57:47 +0200

    js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-console.log("skrypty załadowane!")
+<!doctype html>
+<html lang="en">
+
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<style>
+	
+	</style>
+	<title>Zajęcia nr 3</title>
+</head>
+
+<body>
+	<div id="miejsceNaOwce"></div>
+	<script src="./scripts.js"></script>
+</body>
+
+</html>
